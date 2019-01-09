a8b9aa50e667dd30f653f79bb42c3ad812107c34
fathi-hmeed <fathi@as-hal.net>
2019-01-09 18:26:53 +0300

    login page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AboutUsComponent } from './main-pages/about-us/about-us.component';
+import { LoginComponent } from './core/login/login.component';
 
 const routes: Routes = [
-  {path: 'about-us', component:AboutUsComponent }
+  {path: 'about-us', component:AboutUsComponent },
+  {path: 'login', component:LoginComponent }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './layout/nav-bar/nav-bar.component';
 import { AboutUsComponent } from './main-pages/about-us/about-us.component';
+import { LoginComponent } from './core/login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavBarComponent,
     AboutUsComponent,
+    LoginComponent,
   ],
   imports: [
     BrowserModule,
